2102354662c57ee8b32cc8552d67e69c88e79d86
SidusProxy <gianlucaannina@gmail.com>
2026-06-03 15:47:35 +0200

    "PrenotazioneDTO" classes created

---
+++ src/Ludwig.Data/DTO/Prenotazione.cs
@@ -0,0 +1,10 @@
+﻿namespace Ludwig.Data.DTO;
+
+public record PrenotazioneDTO(int Id, int RisorsaId, int UtenteId, int DottoreId, DateTime Da, DateTime A, String Stato);
+
+public record PrenotazioneCreaDTO(int RisorsaId, int UtenteId, int DottoreId, DateTime Da, DateTime A, String Stato);
+
+public record PrenotazioneAggiornaDTO(int Id, int RisorsaId, int UtenteId, int DottoreId, DateTime Da, DateTime A, String Stato);
+
+
+
